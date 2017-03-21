7096bb3e387df469934bc6cf969a823ac6ce1ab6
joshuathurston <jthurston3@mail.csuchico.edu>
2017-03-21 10:51:57 -0700

    accidently added old files this is the actual initial start

--- form-validation.js
+++ form-validation.js
@@ -5,34 +5,54 @@ function validate(){
 
   if(userEntered.length >= 6)
   {
+    document.getElementById("usernameGroup").classList.remove("has-error");
     document.getElementById("usernameGroup").classList.add("has-success");
+    document.getElementById("usernameError").classList.remove("show-message");
+    document.getElementById("usernameError").classList.add("hidden-message");
   }
-  //else if(userEntered.indexOf(' ') >= 0)
-  //{
-    //Turn the username items red
-    //document.getElementById("usernameGroup").classList.add("has-error");
-    //Show message that there is an error with the username...
-    //document.getElementById("usernameError").innerHTML="Username must not contain whitespaces";
-    //document.getElementById("usernameError").classList.remove("hidden-message");
-    //document.getElementById("usernameError").classList.add("show-message"); 
-  //{
   else
   {
     //Turn the username items red
     document.getElementById("usernameGroup").classList.add("has-error");
     //Show message that there is an error with the username...
-    document.getElementById("usernameError").innerHTML="Username must be more than 6 Characters";
+    document.getElementById("usernameError").innerHTML="Username must be at least 6 characters";
     document.getElementById("usernameError").classList.remove("hidden-message");
-    document.getElementById("usernameError").classList.add("show-message"); 
+    document.getElementById("usernameError").classList.add("show-message");   
   }
-  if(passEntered != "password")
+  if(userEntered.indexOf(' ') >= 0)
   {
-    document.getElementById("passwordGroup").classList.add("has-success");
+    //Turn the username items red
+    document.getElementById("usernameGroup").classList.add("has-error");
+    //Show message that there is an error with the username...
+    document.getElementById("usernameError").innerHTML="can't contain whitespaces";
+    document.getElementById("usernameError").classList.remove("hidden-message");
+    document.getElementById("usernameError").classList.add("show-message");  
+  }
+  
+  if(passEntered != "password" && passEntered != "PASSWORD" && passEntered != userEntered)
+  {
+    if(passEntered.length >= 6 && passEntered.length <= 20)
+    {
+      document.getElementById("passwordGroup").classList.remove("has-error");
+      document.getElementById("passwordGroup").classList.add("has-success");
+      document.getElementById("passwordError").classList.remove("show-message");
+      document.getElementById("passwordError").classList.add("hidden-message");
+    }
+    else
+    {
+      //Show message that there is an error with the password...
+      document.getElementById("passwordError").innerHTML="Password must be between 6-20 characters";
+      document.getElementById("passwordError").classList.remove("hidden-message");
+      document.getElementById("passwordError").classList.add("shown-message");
+      //Turn the password items red
+      document.getElementById("passwordGroup").classList.add("has-error");
+      
+    }
   }
   else
   {
     //Show message that there is an error with the password...
-    document.getElementById("passwordError").innerHTML="Password Must not contain "password"";
+    document.getElementById("passwordError").innerHTML="Password can't be password, PASSWORD, OR " + userEntered;
     document.getElementById("passwordError").classList.remove("hidden-message");
     document.getElementById("passwordError").classList.add("show-message");
     //Turn the password items red
